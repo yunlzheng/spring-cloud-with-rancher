f05748e9182cf415600a8f87845b66814ff8f439
ylzheng <yunl.zheng@gmail.com>
2016-07-10 15:18:24 +0800

    remove target

--- eureka/target/docker/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-PROFILE=${ACTIVE_PROFILE:=default}
-EUREKA_INSTANCE_HOSTNAME=$(curl http://rancher-metadata/latest/self/container/service_name)
-
-echo $EUREKA_INSTANCE_HOSTNAME
-
-java -Xmx512m -Djava.security.egd=file:/dev/./urandom -jar eureka.jar --spring.profiles.active=$PROFILE --eureka.instance.hostname=$EUREKA_INSTANCE_HOSTNAME
